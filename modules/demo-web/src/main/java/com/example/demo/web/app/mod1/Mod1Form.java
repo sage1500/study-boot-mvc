cc98c88872fa387618e4e30d6056b1e0fa4de041
sage1500 <motosage25@gmail.com>
2021-04-12 04:50:10 +0900

    バリデーション追加

--- modules/demo-web/src/main/java/com/example/demo/web/app/mod1/Mod1Form.java
+++ modules/demo-web/src/main/java/com/example/demo/web/app/mod1/Mod1Form.java
@@ -2,7 +2,10 @@ package com.example.demo.web.app.mod1;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -10,15 +13,21 @@ import lombok.Data;
 public class Mod1Form implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @NotEmpty(groups = { Update.class, Delete.class })
+    @Size(max = 0, groups = { Create.class })
     private String id;
 
-    @NotNull(groups = { Create.class, Update.class })
+    @Size(max = 10, groups = { Create.class, Update.class })
     private String prop1;
 
+    @Size(max = 10, groups = { Create.class, Update.class })
     private String prop2;
 
+    @Size(max = 10, groups = { Create.class, Update.class })
     private String prop3;
 
+    @Max(value = 0, groups = { Create.class })
+    @Min(value = 1, groups = { Update.class })
     private long version;
 
     public interface Create {
